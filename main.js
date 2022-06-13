b6f91e03844dd54227c3f527e636e0e4ffbd0063
Fossyman <shadow-eclipse-studios@outlook.com>
2022-06-13 18:16:53 +0100

    V1.3

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const hamburger = document.querySelector('.hamburger');
 if(hamburger){
 hamburger.addEventListener("click", myFunction);
 }
+var scale = Math.min( 
+    availableWidth / contentWidth, 
+    availableHeight / contentHeight 
+  );
 
 function myFunction(){
     this.classList.toggle('is-active');
 }
+
